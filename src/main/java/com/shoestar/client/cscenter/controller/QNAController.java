75780134f793f4329a47dad717cdfb0870eba55a
hyunseok <rkdgustjr94@naver.com>
2019-06-04 14:42:18 +0900

    hyunseok 6/4임시커밋 2:42

--- src/main/java/com/shoestar/client/cscenter/controller/QNAController.java
+++ src/main/java/com/shoestar/client/cscenter/controller/QNAController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,13 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.shoestar.client.cscenter.service.QNAService;
 import com.shoestar.client.cscenter.vo.QNAVO;
+import com.shoestar.client.login.vo.LoginVO;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @RequestMapping("/cscenter/*")
-@SessionAttributes("mem_id")
 @Log4j
 @AllArgsConstructor
 public class QNAController {
@@ -28,8 +29,11 @@ public class QNAController {
 		
 		//1:1문의게시판
 		@RequestMapping(value="/qnaBoard", method=RequestMethod.GET)
-		public String qnaList(@ModelAttribute("data") QNAVO qvo, Model model) {
+		public String qnaList(@ModelAttribute("data") QNAVO qvo, @SessionAttribute("login") LoginVO lvo , Model model) {
 			log.info("qnaList..호출");
+			
+			qvo.setMem_no(lvo.getMem_no());
+			
 			List<QNAVO> qnaList = qnaService.qnaList(qvo); 
 			model.addAttribute("qnaList", qnaList);
 			model.addAttribute("mem_id", "mem_id");
