47ae028c87429b5fa75eb2140a1d662e40633adf
낡은것 <jhbae0117@naver.com>
2019-06-04 22:45:29 +0900

    1.0.14 테이블 수정에 따른 일부 수정 반영

---
+++ src/main/java/com/shoestar/client/prod/vo/ProdStockVO.java
@@ -0,0 +1,12 @@
+package com.shoestar.client.prod.vo;
+
+import lombok.Data;
+
+@Data
+public class ProdStockVO {
+	private int ps_no;
+	private int pi_no;
+	private int ps_size;
+	private int ps_qty;
+	private String ps_status;
+}
